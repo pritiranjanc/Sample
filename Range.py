a26296dd834810eb5c12d6097d84300396ffc22f
ranjanp1 <pritiranjan.c@gmail.com>
2022-07-16 23:04:40 +0530

    Added 1st commit

---
+++ Range.py
@@ -0,0 +1,11 @@
+print("Range function with single Param")
+for i in range(5):
+    print(i)
+
+print("Range function with 3 Param")
+for l1 in range(0, 5, 2):
+    print(l1)
+
+print("Range function with 2 Param")
+for l2 in range(0, 5):
+    print(l2)
